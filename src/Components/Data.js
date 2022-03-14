b20c5d37865363d758c4170173a8ab4ae9939c7a
ZakriyaSoudagar <zakriyasoudagar@gmail.com>
2022-03-14 21:32:10 +0530

    second

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { useState } from "react";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { DataContext } from "./Context";
 import './Style.css';
 
@@ -14,14 +14,93 @@ export const Data =(value)=>
      const [age, setAge] = useState('');
      const [stack, setStack] = useState('');
      const [batch, setBatch] = useState('');
+     const link =useNavigate();
+     const {list}= useParams();
+     console.log(list);
+
+     const add=()=>
+    {
+
+        if(name==""|| age==""||stack==""||batch=="") return alert("Fill all the details")
+        if(list===undefined){
+        setStudents([...students,{
+            id:`${students.length}`,
+            name:name,
+            age:age,
+            stack:stack,
+            batch: batch,
+            update: <Link to={`./desc/${students.length}`}>edit</Link>
+        }
+      ]
+     )
+     
+    }
+    else{
+        setStudents( students.map((element)=>{
+            if(element.id===list)
+            {
+                return {
+                    id: list,
+                    name,
+                    age,
+                    stack,
+                    batch,
+                    update: <Link to={`./desc/${students.length}`}>edit</Link>
+                    
+                }
+
+            }
+
+            else return element;
+        })
+        )
+    }
+    link('/students')
+
+}
+
+        useEffect(()=>{
+            if(!!list)
+            {
+           const student = students.filter((element)=>{
+               return element.id===list 
+
+           })[0]
+
+           setAge(student.age);
+           setName(student.name);
+           setStack(student.stack);
+           setBatch(student.batch);
+        }
+
+        },[])
+
+
+    const Edit=(id)=>
+    {
+        
+        useEffect(()=>{
+            console.log(id);
+       
+        students.map((student)=>{
+            console.log(student);
+            if(id===student.id)
+            {
+                console.log(student);
+                setStudents({
+                    name:student.name,
+                    age:student.age,
+                    stack:student.stack,
+                    batch:student.batch
+                })
+            }
+        })
+    })
+        link('/students/desc/'+id)
+}
+
    
     
-
-    
-
-   
-    
-
 
     return(
         <div>
@@ -37,7 +116,7 @@ export const Data =(value)=>
             
         </div>
         <div>
-        <button className="btn"  onClick={()=>value()}>Add Student</button>
+        <button className="btn"  onClick={add}>Add Student</button>
         </div>
         </div>
     )
--- src/Components/Student.js
+++ src/Components/Student.js
@@ -8,13 +8,10 @@ import { Data } from "./Data";
 export const Student = ()=>
 {
     
-     const [students,setStudents]=useContext(DataContext);
-     const [name, setName] =useState('');
-     const [age, setAge] = useState('');
-     const [stack, setStack] = useState('');
-     const [batch, setBatch] = useState('');
-     const link= useNavigate();
-     const navigate =useNavigate();
+     const [students]=useContext(DataContext);
+    
+     
+     
      console.log(students);
 
     const columns =[
@@ -39,63 +36,26 @@ export const Student = ()=>
     field:'update'
 }   ]
 
-const add=()=>
-    {
-        if(name==""|| age==""||stack==""||batch=="") return alert("Fill all the details")
-        setStudents([...students,{
-            id:`${students.length}`,
-            name:name,
-            age:age,
-            stack:stack,
-            batch: batch,
-            update: <button to={`${students.length}`} onClick={()=>Edit(`${students.length}`)}>edit</button>
-        }
-      ]
-     )
-     navigate('/students');
-    }
 
 
 
-    const Edit=(id)=>
-    {
-        
-        useEffect(()=>{
-            console.log(id);
-       
-        students.map((student)=>{
-            console.log(student);
-            if(id===student.id)
-            {
-                console.log(student);
-                setStudents({
-                    name:student.name,
-                    age:student.age,
-                    stack:student.stack,
-                    batch:student.batch
-                })
-            }
-        })
-    })
-        link('/students/desc/'+id)
 
+
+    
   
         
     //    link(`/students/${students[0].name}`) 
-    }
+    
 
 
-const submit = ()=>
-{
-     <Data value={add}/>
-}
+
 
 
     return(
         <div>
             <div>
                 <li><h1>Students Data</h1></li>
-                <li><Link to='desc' onClick={submit} className="btn">Add Student</Link></li>
+                <li><Link to='desc'  className="btn">Add Student</Link></li>
             </div>
             <MaterialTable  title='' data={students} columns={columns} 
             style={{width:'99vw', marginTop:'25px'}}
